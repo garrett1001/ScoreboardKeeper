65201e608ce75a5cc26b9a5644ee1041b476901e
garrett1001 <gwelton@mail.csuchico.edu>
2018-04-12 21:21:19 -0700

    uses function

--- main.cpp
+++ main.cpp
@@ -8,48 +8,58 @@ const int MAX_TEAMS = 4;
 const int MIN_PERIODS = 1;
 const int MIN_TEAMS = 1;
 
+void printScoreboard(vector < vector <int> >);
+
 int main()
 {
-  int periods;
-  int teams;
-  vector<vector<int> > board;
+        int periods;
+        int teams;
+        vector<vector<int> > board;
 
-  cout<<"How many competitors? ";
-  cin>>teams;
-  cout<<"How many scoring periods? ";
-  cin>>periods;
+        cout<<"How many competitors? ";
+        cin>>teams;
+        cout<<"How many scoring periods? ";
+        cin>>periods;
 
-  if( teams < MIN_TEAMS || periods < MIN_PERIODS || 
-      teams > MAX_TEAMS || periods > MAX_PERIODS )
-  {
-    cout<<"Must have between "<<MIN_TEAMS<<" and "<<MAX_TEAMS<<" competitors.\n";
-    cout<<"and between "<<MIN_PERIODS<<" and "<<MAX_PERIODS<<" periods.\n";
-    return 0;
-  }
-  else
-  {
-   //make scoreboard and fill it with zeros
-        cout<<"SCOREBOARD"<<endl;
-       
-        for ( int i = 0; i < teams; i++ )
+        if( teams < MIN_TEAMS || periods < MIN_PERIODS || 
+                        teams > MAX_TEAMS || periods > MAX_PERIODS )
         {
-              vector<int> temp;
-              for ( int j = 0; j < periods; j++ )
-              {
-                    temp.push_back(0);
-              }
-              board.push_back(temp);
+                cout<<"Must have between "<<MIN_TEAMS<<" and "<<MAX_TEAMS<<" competitors.\n";
+                cout<<"and between "<<MIN_PERIODS<<" and "<<MAX_PERIODS<<" periods.\n";
+                return 0;
         }
-   //once created, display the scoreboard
-        for ( int i = 0; i < teams; i++ )
+        else
         {
-              cout<<"Player "<<i+1<<": ";
-              for ( int j = 0; j < periods; j++ )
-              {
-                    cout<<board[i][j]<<"|";
-              }
-              cout<<endl;
+                //make scoreboard and fill it with zeros
+                for ( int i = 0; i < teams; i++ )
+                {
+                        vector<int> temp;
+                        for ( int j = 0; j < periods; j++ )
+                        {
+                                temp.push_back(0);
+                        }
+                        board.push_back(temp);
+                }
+                //once created, display the scoreboard
+                printScoreboard(board);
         }
-  }
-  return 0;
+        return 0;
 }
+
+void printScoreboard(vector< vector<int> > grid)
+{
+        cout<<"SCOREBOARD\n";
+
+        for ( int i = 0; i < grid.size(); i++ )
+        {
+                cout<<"Player "<<i+1<<": ";
+                for ( int j = 0; j < grid[i].size(); j++ )
+                {
+                        cout<<grid[i][j]<<"|";
+                }
+                cout<<endl;
+        }
+}
+
+
+
